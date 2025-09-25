57d90384ae02d71730ec936c7dacc68563ab7dc8
Fanathing <mmybonnie@gamil.com>
2025-09-25 09:49:44 +0900

    Feat: server.js 에서  요청시 index.html을 응답하도록 구현

--- server.js
+++ server.js
@@ -1,2 +1,11 @@
 const express = require("express");
 const app = express();
+const path = (`${__dirname}/views/boards`)
+
+app.get("/", (req,res) => {
+    res.sendFile(`${path}/index.html`)
+})
+
+app.listen(3000, () => {
+    console.log("월별평가의 시계는 돌아간다..!");
+})

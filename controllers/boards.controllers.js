f12ac3cf3a936b54bb84163c39eef0dc2bc30240
Fanathing <mmybonnie@gamil.com>
2025-09-25 11:07:31 +0900

    Feat: create.html에서 post 요청 발생 시 boards 배열에 작성된 글 내용을 push할 수 있도록 구현

--- controllers/boards.controllers.js
+++ controllers/boards.controllers.js
@@ -8,6 +8,30 @@ const getList = (req,res) => {
     })
 }
 
+const getCreate = (req,res) => {
+    res.render("boards/create.html")
+}
+
+const postCreate = (req,res) => {
+   const { user_id, writer, title, content } = req.body;
+   const now = new Date();
+   const date = `${now.getFullYear()}-${now.getMonth()+1}-${now.getDate()}`
+
+   boards.push({
+    id: boards.length +1,
+    user_id: user_id,
+    writer: writer,
+    title: title,
+    content: content,
+    hit: 0,
+    created_at:date
+   });
+   
+   res.redirect("/boards");
+}
+
 module.exports = {
-    getList
+    getList,
+    getCreate,
+    postCreate
 };
--- router/boards.router.js
+++ router/boards.router.js
@@ -5,5 +5,6 @@ const router = express.Router();
 
 router.get("/", boardsControllers.getList);
 router.get("/create", boardsControllers.getCreate);
+router.post("/create", boardsControllers.postCreate);
 
 module.exports = router;

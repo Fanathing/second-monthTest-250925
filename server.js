68ec05171642b43bf27bb76f2eb0df9f3ed11e3c
Fanathing <mmybonnie@gamil.com>
2025-09-25 10:02:18 +0900

    Feat: 라우터,컨트롤러스,데이터 폴더 만든 뒤 data 초기완료 및 server.js에서 router사용 설정, 바디파서 설정,넌적스 및 익스프레스 연결 설정

--- server.js
+++ server.js
@@ -1,7 +1,21 @@
 const express = require("express");
 const app = express();
-const path = (`${__dirname}/views/boards`)
+const path = (`${__dirname}/views/boards`);
+const nunjucks = require("nunjucks");
+// 언랭코디드를 파싱해서 가져오겠다는 설정
+app.use(express.urlencoded({ extended: false }));
 
+const boardsRouter = (`./router/boards.router.js`);
+
+app.use("/boards", boardsRouter)
+
+// 넌적스와 익스프레스를 연결(템플린엔진 사용)
+app.set("view engine", "html");
+nunjucks.configure("views", {
+    express: app
+})
+
+// "/" 요청시 메인페이지인 index.html 응답
 app.get("/", (req,res) => {
     res.sendFile(`${path}/index.html`)
 })
